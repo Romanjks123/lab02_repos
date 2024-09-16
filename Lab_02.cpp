7b71ec3cbdbe8915660f7bc7af99181fa6a7ab0a
Romanjks123 <m7playyt@gmail.com>
2024-09-16 18:11:55 +0300

    first branch_02 commit

--- Lab_02.cpp
+++ Lab_02.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main()
 {
 	double a;
-	double z1;
+	//double z1;
 	double z2;
 
 	cout << "a = "; cin >> a;
 
-	z1 = ((sin(a) + sin(a * 5)) - sin(3 * a)) / ((cos(a) - cos(3 * a)) - cos(5 * a));
+	//z1 = ((sin(a) + sin(a * 5)) - sin(3 * a)) / ((cos(a) - cos(3 * a)) - cos(5 * a));
 	z2 = (sin(3 * a) / cos(3 * a));
 
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cout << endl;
